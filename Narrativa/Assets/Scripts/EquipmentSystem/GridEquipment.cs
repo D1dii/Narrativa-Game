87c471ae2833f44ec6994547ef39d9f3b866e2eb
D1dii <didacgp123@gmail.com>
2026-04-29 17:20:30 +0200

    Grid Visual

--- Narrativa/Assets/Scripts/EquipmentSystem/GridEquipment.cs
+++ Narrativa/Assets/Scripts/EquipmentSystem/GridEquipment.cs
@@ -5,8 +5,6 @@ public struct Cell
 {
     public int x;
     public int y;
-    public float width;
-    public float height;
 }
 
 public class GridEquipment : MonoBehaviour
@@ -14,28 +12,36 @@ public class GridEquipment : MonoBehaviour
     [SerializeField] private int rows;
     [SerializeField] private int columns;
 
-    [SerializeField] private Cell[] cells;
+    [SerializeField] private Cell[,] cells;
 
-    [SerializeField] private GameObject cellImage;
+    [SerializeField] private GameObject cellImagePrefab;
 
     private void Awake()
     {
-        
+        InitializeGrid();
     }
 
     public void InitializeGrid()
     {
-        cells = new Cell[rows * columns];
+        cells = new Cell[rows, columns];
 
-        for (int i = 0; i < cells.Length; i++)
+        for (int i = 0; i < columns; i++)
         {
-            cells[i] = new Cell();
-            cells[i].x = i;
+            for (int j = 0; j < rows; j++)
+            {
+                cells[i, j] = new Cell();
+                cells[i, j].x = i;
+                cells[i, j].y = j;
+                DrawCell(i, j);
+            }
         }
     }
 
-    private void DrawGrid()
+    public void DrawCell(int i, int j)
     {
-        
+        var cellImage = Instantiate(cellImagePrefab);
+        cellImage.transform.SetParent(transform);
+        cellImage.GetComponent<RectTransform>().localPosition = new Vector3(-180 + 70 * i, 150 - 70 * j, 0);
+
     }
 }
