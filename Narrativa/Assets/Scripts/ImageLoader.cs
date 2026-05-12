985fa9a27de010d5e9ab20825aa11507b33d559a
Àlex Becerra García <abecerragarcia04@gmail.com>
2026-05-12 19:18:43 +0200

    Arte hombre misterioso

--- Narrativa/Assets/Scripts/ImageLoader.cs
+++ Narrativa/Assets/Scripts/ImageLoader.cs
@@ -1,30 +1,71 @@
 using UnityEngine;
+using UnityEngine.UI;
 using Yarn.Unity;
+using System.Collections.Generic;
 
 public class ImageLoader : MonoBehaviour
 {
+    [System.Serializable]
+    public struct DialogueSprite
+    {
+        public string spriteName;    
+        public Sprite sprite;      
+    }
 
-    [SerializeField] private Canvas canvasToActivate;
+    [Header("Referencias UI")]
+    [SerializeField] private GameObject imageContainer; 
+    [SerializeField] private Image characterImage;      
+    [Header("Configuración de Sprites")]
+    [SerializeField] private List<DialogueSprite> sprites = new List<DialogueSprite>();
 
-    public static Canvas myCanvas;
+    private Dictionary<string, Sprite> spriteDictionary = new Dictionary<string, Sprite>();
+
+    private static ImageLoader instance;
 
     private void Awake()
     {
-        myCanvas = canvasToActivate;
+        instance = this;
+
+        foreach (var s in sprites)
+        {
+            if (!spriteDictionary.ContainsKey(s.spriteName))
+            {
+                spriteDictionary.Add(s.spriteName, s.sprite);
+            }
+        }
+
+        if (imageContainer != null)
+        {
+            imageContainer.SetActive(false);
+        }
     }
 
-    [YarnCommand("ShowImageOnScreen")]
-    public static void ShowHiddenCanvas()
+    [YarnCommand("ShowSprite")]
+    public static void ShowSprite(string spriteName)
     {
-        if (myCanvas != null)
+        if (instance == null) return;
+
+        if (instance.spriteDictionary.TryGetValue(spriteName, out Sprite spriteToShow))
         {
-            Debug.Log("Activate Canvas");
-            myCanvas.gameObject.SetActive(true);
+            instance.characterImage.sprite = spriteToShow;
+            instance.imageContainer.SetActive(true);
+            Debug.Log($"Sprite cargado: {spriteName}");
         }
         else
         {
-            Debug.LogWarning("Canvas reference is missing!");
+            Debug.LogWarning($"¡No se encontró el sprite '{spriteName}' en la lista del Inspector!");
         }
+    }
 
+    [YarnCommand("HideSprite")]
+    public static void HideSprite()
+    {
+        if (instance == null) return;
+
+        if (instance.imageContainer != null)
+        {
+            instance.imageContainer.SetActive(false);
+            Debug.Log("Sprite oculto");
+        }
     }
 }
